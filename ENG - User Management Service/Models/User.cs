7b264420b7f8da72e745215edd5d9925e9ff4409
1Pampu <piampianimartin@gmail.com>
2024-04-16 18:19:14 -0300

    user model

---
+++ ENG - User Management Service/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ENG___User_Management_Service.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
