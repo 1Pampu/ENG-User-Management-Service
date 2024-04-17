4c0e25b90f2e412607d67607caad257dea8bd0ff
1Pampu <piampianimartin@gmail.com>
2024-04-16 21:05:33 -0300

    Conection to database & basic routes

---
+++ ENG - User Management Service/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ENG___User_Management_Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ENG___User_Management_Service.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
